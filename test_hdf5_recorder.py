898948b89b7eaaaa70a836fed0ba4d1e15cbeb8c
Sidney Cadot <sidney@jigsaw.nl>
2022-07-08 11:43:28 +0200

    Added some remarks about the correct way of creating record type instances.

--- test_hdf5_recorder.py
+++ test_hdf5_recorder.py
@@ -11,7 +11,7 @@ from hdf5_recorder import ActiveHDF5Recorder
 def main() -> None:
     """Test the ActiveHDF5Recorder."""
 
-    imu_dtype = np.dtype([
+    record_dtype = np.dtype([
         ("jan"     , np.float64),
         ("piet"    , np.uint32 ),
         ("joris"   , np.uint32 ),
@@ -32,17 +32,25 @@ def main() -> None:
             test_percentage = frame_index / num_frames * 100.0
             print("[{:7.3f} %] frame: {:10d} queue: {:10d}".format(test_percentage, frame_index, recorder._queue.qsize()))
 
-            # Make test frame and test IMU data.
+            # Make test frame data.
             frame = np.zeros((2500, 2500), dtype=np.uint16) + frame_index
             frame_time = time.time()
-            imu_record = np.array((frame_index, frame_index, frame_index, True), dtype=imu_dtype)
-            imu_record_time = time.time()
+
+            # Make test record data.
+            # Note that the creation of a single record is done by calling np.array on a tuple.
+            # This is important, as this will create a 0-dimensional (ie scalar) record instance, as intended;
+            # Somewhat surprisingly, when passing a list as the first argument, this doesn't work as intended.
+            record = np.array((frame_index, frame_index, frame_index, True), dtype=record_dtype)
+
+            # Verify that the record is, in fact, 0-dimensional as intended.
+            assert len(record.shape) == 0
+            record_time = time.time()
 
             # Push them to the recorder.
             recorder.store("frames", frame)
             recorder.store("frame_time", frame_time)
-            recorder.store("imu_records", imu_record)
-            recorder.store("imu_record_time", imu_record_time)
+            recorder.store("records", record)
+            recorder.store("record_time", record_time)
 
 
 if __name__ == "__main__":
